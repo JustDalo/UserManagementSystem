75447cc51abd32e6f6845e3ce37867eca02aca98
JustDalo <shishlodan@mail.ru>
2022-04-30 14:59:34 +0300

    init commit

---
+++ src/main/kotlin/com/dalo/usermanagementsystem/UserManagementSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.dalo.usermanagementsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserManagementSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<UserManagementSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/dalo/usermanagementsystem/UserManagementSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dalo.usermanagementsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserManagementSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
